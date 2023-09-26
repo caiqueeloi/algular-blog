810c73d45d9461213e3cfbf66b895c1979283398
Caique Eloi <caiquesantoos23@gmail.com>
2023-09-26 13:44:36 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,36 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Lamborghini revela nome e visual de seu 1° carro elétrico.",
+
+    "description": "Como já revelou a Lamborghini, a ideia é que o novo Lanzador seja um carro para o dia a dia, que une esportividade e conforto, sendo mais prático que o Huracán e o Revuelto. Além disso, no que diz respeito à motorização, a marca ainda não revelou muita coisa. Sabe-se somente que o o modelo terá motores duplos com uma potência específica e elevada, além de tração integral. A potência total deve ficar na casa dos quatro dígitos, próximo dos 1.300cv. O Lamborghini Lanzador contará também com sistema de suspensão ativa, com molas pneumáticas, amortecedores adaptativos e direção do eixo traseiro. Até 2024, todo o portfólio da Lamborghini será eletrificado. O modelo será o primeiro carro 100% elétrico da marca, mas não o primeiro eletrificado, uma vez que a Lamborghini já conta, atualmente, com um modelo híbrido, o Lamborghini Revuelto, que tem motorização PHEV e é, na verdade, um substituo híbrido do Aventador. Além disso, até 2024, outros dois híbridos também chegarão ao mercado: o Urus e o Huracán, os quais, até então, estão disponíveis apenas com motorização à combustão. Assim, até o fim do ano que vem, a expectativa é que todos os Lamborghini sejam eletrificados de alguma forma.",
+
+    "photoCover":"https://uploads.vrum.com.br/2023/08/e97fedc5-lanzador-1-741x417.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Novo mercedes-AMG GT tem V8 de 585 cv para rivalizar com Porshe 911",
+
+    "description": "Como adiantado em 2021, agora o GT será exclusivamente cupê. A versão conversível foi substituída pelo Mercedes-AMG SL. Mas as mudanças ficam apenas no nome e na carroceria, já que a montadora adiantou que boa parte das mudanças trazidas para o GT, também serão usadas n roadster.... Entre essas novidades em comum está o novo chassi. A estrutura desenvolvida pela Mercedes é feita de alumínio, aço, magnésio e compósitos de fibra de vidro e carbono. As melhorias podem ser vistas, principalmente, no espaço interno para dois passageiros dos bancos traseiros e na capacidade do porta-malas, que pode variar entre 321 litros e 675 litros. Ele também ficou maior, mais largo e mais alto. No comprimento, o cupê ganhou 1,82 cm, chegando a 4,73 cm, enquanto sua distância entre-eixos aumentou em 7 cm, totalizando 2,7 m. Suas outras medidas são 1,98m de largura (aumento de 4,5cm) e 1,35 m de altura (aumento de 6,6cm).",
+
+    "photoCover":"https://berthagroup.com.br/wp-content/uploads/2020/07/amg4-1.gif"
+  },
+
+  {
+    "id":"3",
+    "title": "Novo BMW X6 M chega ao Brasil até o fim do ano, diz a fabricante",
+
+    "description": "O X6 M  vem equipado com um motor 4.4 V8 de 600 cv e 76,5 kgfm. Combinado ao câmbio automático de oito marchas e ao sistema de tração integral, acelera de 0-100 km/h em 3,8 segundos e atinge os 290 km/h de velocidade máxima. A lista de equipamentos do BMW terá como destaques o sistema de visão noturna com detector de pedestres, direção semiautônoma e som premium Harman Kardon. Posicionado na linha abaixo apenas do X7 , o X6 chegou ao Brasil em março e está disponível atualmente apenas na versão xDrive40i M Sport, que está equipada com um motor 3.0, de seis cilindros e 340 cv. O preço surgerido parte de R$ 567.950. A BMW lançou este mês no mercado brasileiro um showroom virtual dos veículos da marca. Disponível no site (www.bmw.com.br) e no canal oficial da empresa no YouTube, traz vídeos com duração média de três minutos com os principais destaques de cada produto da linha. No site é possível ainda consultar informações técnicas e configurar cada carro de acordo com a sua preferência. Veja baixo o vídeo oficial de lançameno do SUV.",
+
+    "photoCover":"https://cdn.motor1.com/images/mgl/0eVqvn/s3/2023-bmw-x6.jpg"
+  },
+
+  {
+    "id":"4",
+    "title": "Porsche garante: 911 terá motor a combustão até quando for possível",
+
+    "description": "A Porsche acaba de anunciar a chegada da nova geração do Porsche 911 GT3 RS, versão mais apimentada de um de seus principais esportivos atualmente. Segundo a montadora de Stuttgart, o bólido está mais rápido graças a algumas modificações em termos de aerodinâmica e tecnologia, que fizeram seu conhecido motor aspirado falar mais alto. Muitos esperavam que a nova geração do 911 GT3 RS pudesse ser equipada com um câmbio manual, mas a Porsche não quis. Ele vem com um conhecido câmbio automatizado de dupla embreagem com sete velocidades, que ajuda demais o motorzão 4.0 com seis cilindros de 525cv ir de 0 a 100 km/h em 3,2 segundos, com velocidade máxima de 296 km/h.  Segundo a marca, esse é o esportivo de rua mais rápido a utilizar um motor aspirado, ou seja, sem ajuda de turbocompressor.",
+
+    "photoCover":"https://t.ctcdn.com.br/Mlz2WmuoZATUWZtI-rlAsXvlCm4=/1024x0/smart/filters:format(webp)/i623065.jpeg"
+  },
+]
